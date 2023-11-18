dd7226033a35fc28e37a00714741fd0ea5b6a98c
Jonathan Giorgi Silveira <jonathan.silveira-ext@ab-inbev.com>
2023-11-18 12:23:49 -0300

    Add MediaTypeEnum to result models

--- domain/src/main/java/com/silveira/jonathang/android/domain/model/TvShowsSectionModel.kt
+++ domain/src/main/java/com/silveira/jonathang/android/domain/model/TvShowsSectionModel.kt
@@ -1,6 +1,10 @@
 package com.silveira.jonathang.android.domain.model
 
+import com.silveira.jonathang.android.domain.model.MediaTypeEnum.TV
+
 data class TvShowsSectionModel(
     override val title: String,
     val items: List<TvShowResultModel>
-) : SearchSectionModel
+) : SearchSectionModel {
+    override val mediaType: MediaTypeEnum = TV
+}
