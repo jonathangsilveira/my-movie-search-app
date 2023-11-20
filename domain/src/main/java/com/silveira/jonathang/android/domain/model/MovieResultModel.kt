82090a2f8df605d1fc004f688c08c7f1dd6b529b
Jonathan Giorgi Silveira <jonathan.silveira-ext@ab-inbev.com>
2023-11-20 13:49:28 -0300

    Change property to nullable String

--- domain/src/main/java/com/silveira/jonathang/android/domain/model/MovieResultModel.kt
+++ domain/src/main/java/com/silveira/jonathang/android/domain/model/MovieResultModel.kt
@@ -5,10 +5,10 @@ import com.silveira.jonathang.android.domain.model.MediaTypeEnum.MOVIE
 data class MovieResultModel(
     override val id: Int,
     val title: String,
-    val posterPath: String,
     val popularity: Double,
     val originalLanguage: String,
-    val releaseDate : String
-) : SearchResultModel {
+    val releaseDate : String,
+    val posterPath: String?
+    ) : SearchResultModel {
     override val mediaType: MediaTypeEnum = MOVIE
 }
