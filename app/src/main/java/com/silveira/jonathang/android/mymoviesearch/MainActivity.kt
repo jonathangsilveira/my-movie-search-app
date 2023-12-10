0085ca3b10de2fd650a60f3dce84e6d37248af88
Jonathan Giorgi Silveira <jonathan.silveira-ext@ab-inbev.com>
2023-12-10 18:00:41 -0300

    React to ViewState changes

--- app/src/main/java/com/silveira/jonathang/android/mymoviesearch/MainActivity.kt
+++ app/src/main/java/com/silveira/jonathang/android/mymoviesearch/MainActivity.kt
@@ -5,17 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -23,8 +25,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.tooling.preview.Preview
-import com.silveira.jonathang.android.mymoviesearch.ui.theme.MyMovieSearchAppTheme
+import androidx.compose.ui.unit.dp
+import com.silveira.jonathang.android.presentation.compose.SearchResultsContent
+import com.silveira.jonathang.android.presentation.theme.MyMovieSearchAppTheme
 import org.koin.androidx.compose.koinViewModel
 
 class MainActivity : ComponentActivity() {
@@ -69,22 +72,43 @@ fun SearchScreen() {
                     .fillMaxWidth()
                     .wrapContentSize()
             )
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(8.dp)
+            )
+            val viewState: SearchViewState by viewModel.viewState.collectAsState()
+            when (viewState) {
+                SearchViewState.EmptyState -> Text(
+                    text = "Search for: Movies, TV Shows and People!",
+                    modifier = Modifier.wrapContentSize()
+                )
+
+                SearchViewState.Fetching -> Text(
+                    text = "Loading...",
+                    modifier = Modifier.wrapContentSize()
+                )
+
+                SearchViewState.GenericError ->
+                    Text(
+                        text = "Something went wrong",
+                        modifier = Modifier.wrapContentSize()
+                    )
+
+                SearchViewState.NoResults ->
+                    Text(
+                        text = "No results found!",
+                        modifier = Modifier.wrapContentSize()
+                    )
+
+                is SearchViewState.Success -> SearchResultsContent(
+                    sections = (viewState as SearchViewState.Success).sections,
+                    onItemClick = viewModel::onItemClicked,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight()
+                )
+            }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyMovieSearchAppTheme {
-        Greeting("Android")
     }
 }
