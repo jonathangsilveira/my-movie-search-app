27f5612711073cdce66cc32a29278e7135618810
Jonathan Giorgi Silveira <jonathan.silveira-ext@ab-inbev.com>
2023-12-16 13:47:23 -0300

    Apply nullable to optional values

--- remote/src/main/java/com/silveira/jonathang/android/remote/response/PersonResultResponse.kt
+++ remote/src/main/java/com/silveira/jonathang/android/remote/response/PersonResultResponse.kt
@@ -9,7 +9,7 @@ data class PersonResultResponse(
     override val id: Int,
     val name: String,
     val popularity: Double,
-    @SerialName("profile_path") val profilePath: String,
-    @SerialName("known_for_department") val knownForDepartment: String,
-    @SerialName("known_for")val knownFor: List<KnownForResponse>
+    @SerialName("profile_path") val profilePath: String?,
+    @SerialName("known_for_department") val knownForDepartment: String?,
+    @SerialName("known_for")val knownFor: List<KnownForResponse>? = null
 ) : SearchResultResponse
